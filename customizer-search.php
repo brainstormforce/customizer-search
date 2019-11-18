e65a8e2f8ca46c70aa98d409a4054fae54e0a291
Nikhil Chavan <email@nikhilchavan.com>
2019-11-18 12:34:12 +0530

    Update plugin to v1.1.3

--- customizer-search.php
+++ customizer-search.php
@@ -7,12 +7,12 @@
  * Author URI:      https://www.brainstormforce.com/
  * Text Domain:     customizer-search
  * Domain Path:     /languages
- * Version:         1.1.2
+ * Version:         1.1.3
  *
  * @package         Customizer_Search
  */
 
-define( 'BSFCS_VER', '1.1.2' );
+define( 'BSFCS_VER', '1.1.3' );
 define( 'BSFCS_DIR', plugin_dir_path( __FILE__ ) );
 define( 'BSFCS_URL', plugins_url( '/', __FILE__ ) );
 define( 'BSFCS_PATH', plugin_basename( __FILE__ ) );
